36df95cd1ef02d3aa7ce08c0286e5fbbcd07e728
Dua Helal <130730242+dua74@users.noreply.github.com>
2024-08-01 23:00:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let signUp = document.getElementById("sign");
+let showDiv = document.getElementById("frm");
+signUp.addEventListener("click", function () {
+  showDiv.style.display = "flex";
+  showDiv.style.alignContent = "center";
+  showDiv.style.justifyContent = "center";
+  // document.getElementById("bd").style.opacity = 0.2;
+  // showDiv.style.opacity = 1;
+  // document.querySelectorAll("tst")[0].style.display = "block";
+});
